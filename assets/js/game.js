8522bc392e56d19f816b0a29cfa4e710c6177bf4
crainone4037 <rainonecarmine0@gmail.com>
2022-05-18 19:03:54 -0400

    console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("this is an alert javascript is running"); 
+function fight(){
+    window.alert("the fight has begun");
+}
+
+fight();
+
+window.prompt("what is your robots name?");
+
+var playerName = window.prompt("what is your robots name?");
+console.log(playerName);
+console.log("our player name is" + playerName );
+
